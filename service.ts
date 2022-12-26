1270f04feaf3d51707055534406ae4597493f206
Yauheni Shypila <Yauheni_Shypila@epam.com>
2022-12-26 17:18:59 +0300

    feat: add 2 task implementation

--- service.ts
+++ service.ts
@@ -1,33 +1,69 @@
 import express from 'express';
 import crypto from 'node:crypto';
+import bodyParser from 'body-parser';
+import { createValidator } from 'express-joi-validation';
+import {
+  createUserBodySchema,
+  paramsIDSchema,
+  paramsSubstringLimitSchema,
+  updateUserBodySchema,
+} from './schemas';
+import type { User } from './types';
 
 const app = express();
+const validator = createValidator();
 
 app.listen(3000);
 
-type User = {
-    id: string;
-    login: string;
-    password: string;
-    age: number;
-    isDeleted: boolean;
-}
+const users: User[] = [];
 
-const users: User[] = [
-    {
-        id: crypto.randomUUID(),
-        login: 'string',
-        password: 'string',
-        age: 10,
-        isDeleted: true,
-}
-];
+app.use(bodyParser.json());
 
+// get all users
 app.get('/users', (req, res) => {
   res.json(users);
 });
 
-app.get('/user/:id', (req, res) => {
-    
-    res.json(req.params.id);
-  });
+// get user by ID
+app.get('/user/:id', validator.params(paramsIDSchema), (req, res) => {
+  res.json(users.filter((user) => user.id === req.params.id));
+});
+
+// create user
+app.post('/user', validator.body(createUserBodySchema), (req, res) => {
+  const user = { ...req.body, id: crypto.randomUUID() };
+  users.push(user);
+  res.json(user);
+});
+
+//update user
+app.put(
+  '/user/:id',
+  validator.params(paramsIDSchema),
+  validator.body(updateUserBodySchema),
+  (req, res) => {
+    const index = users.findIndex((user) => user.id === req.params.id);
+    users[index] = { ...users[index], ...req.body };
+    res.json(users[index]);
+  }
+);
+
+//delete user
+app.put('/user/delete/:id', validator.params(paramsIDSchema), (req, res) => {
+  const index = users.findIndex((user) => user.id === req.params.id);
+  users[index] = { ...users[index], isDeleted: true };
+  res.json(users[index]);
+});
+
+// filter users on limit
+app.get(
+  '/users/:loginSubstring/:limit',
+  validator.params(paramsSubstringLimitSchema),
+  (req, res) => {
+    const filteredUsers = users.filter((user) =>
+      user.login.includes(req.params.loginSubstring)
+    );
+    const sortedUsers = filteredUsers.sort();
+    res.json(sortedUsers.splice(0, +req.params.limit));
+  }
+);
